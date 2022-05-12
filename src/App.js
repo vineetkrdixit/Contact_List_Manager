8fee3adc00f3712ea1f447a446d575ff4d5ea67e
vineetkrdixit <vineetdixit.vns@gmail.com>
2022-05-12 23:09:08 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
-    
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Header/Header.css";
+
+export default function Header() {
+  return (
+    <div>
+      <div className="container header">
+        <h1>Contact Manager</h1>
+      </div>
+    </div>
+  );
+}
